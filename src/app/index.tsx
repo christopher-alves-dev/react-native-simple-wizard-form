f517e0a438ceb6903e46fcb57583f0d533bc5eaa
Christopher Alves <christopher.alves@portalsolar.com.br>
2024-02-07 09:14:03 -0300

    feat: init project and configure native wind

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Home() {
+  return (
+    <View className="bg-blue-500 flex-1 items-center justify-center">
+      <Text className="text-white text-3xl">Hello World</Text>
+    </View>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/app/**/*.{ts,tsx}","./src/components/**/*.{ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
